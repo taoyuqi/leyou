362a2aa65236b5149f29e98e361fa9a8ad368bb9
taoyuqi <76639475+taoyuqi@users.noreply.github.com>
2020-12-27 16:05:34 +0800

    Update User.java
    在线更新

--- leyou-user/leyou-user-interface/src/main/java/com/leyou/user/pojo/User.java
+++ leyou-user/leyou-user-interface/src/main/java/com/leyou/user/pojo/User.java
@@ -87,6 +87,7 @@ public class User {
     public String toString() {
         System.out.println("主干添加");
         System.out.println("分支添加");
+         System.out.println("在线更新");
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
