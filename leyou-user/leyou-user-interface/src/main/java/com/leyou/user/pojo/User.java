52b3925178913304f1d5e113006f5af52f54add7
taoyuqi <2605829249@qq.com>
2020-12-27 16:07:02 +0800

    测试在线更新

--- leyou-user/leyou-user-interface/src/main/java/com/leyou/user/pojo/User.java
+++ leyou-user/leyou-user-interface/src/main/java/com/leyou/user/pojo/User.java
@@ -88,6 +88,7 @@ public class User {
         System.out.println("主干添加");
         System.out.println("分支添加");
          System.out.println("在线更新");
+        System.out.println("Hello Git!");
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
